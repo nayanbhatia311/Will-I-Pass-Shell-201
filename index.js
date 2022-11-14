84175b8353c0f5451b2f9c2ef5fb4c9b8f6b2e86
Nayan Bhatia <nbhatia3@ucsc.edu>
2022-11-13 22:03:11 -0800

    add comma logic for ma avg score

--- index.js
+++ index.js
@@ -8,9 +8,16 @@
     function validateForm(){
         let midterm1Marks=document.forms["getUserMarks"]["midterm1"].value;
         let midterm2Marks=document.forms["getUserMarks"]["midterm2"].value;
-        let maMarks=document.forms["getUserMarks"]["ma_array"].value/30*100;
+        let maMarks=document.forms["getUserMarks"]["ma_array"].value;
+        maMarksArray=maMarks.split(",").map(Number);
+        maMarks_avg=maMarksArray.reduce((a, b) => a + b, 0) / maMarksArray.length;
+        maMarks_avg=maMarks_avg*100/30
+        // alert(maMarks_avg);
         
-        let finalMarksRequired=(80-(maMarks*0.10 + midterm1Marks*0.25 + midterm2Marks*0.25))/0.4
+        
+        // let maMarks_avg=maMarks/30*100;
+        
+        let finalMarksRequired=(80-(maMarks_avg*0.10 + midterm1Marks*0.25 + midterm2Marks*0.25))/0.4
         alert(finalMarksRequired);
         
     }
@@ -19,9 +26,9 @@
 <body>
     <form name="getUserMarks" onsubmit="return validateForm()">  
         <h1>Will I pass SHel's CSE 201 class? Let's find out!</h1>
-        <input type="text" name="midterm1" placeholder="Midterm 1 Marks(out of 100)"> </br> </br>
-        <input type="text" name="midterm2" placeholder="Midterm 2 Marks(out of 100)"> </br> </br>
-        <input type="text" name="ma_array" placeholder="MA's score till now(out of 30) "> </br></br>
+        <input type="text" name="midterm1" placeholder="Midterm 1 Marks(out of 100)" required> </br> </br>
+        <input type="text" name="midterm2" placeholder="Midterm 2 Marks(out of 100)" required> </br> </br>
+        <input type="text" name="ma_array" placeholder="MA's score till now(out of 30) required"> </br></br>
         <input type="submit" value="Submit">
         
     </form>
